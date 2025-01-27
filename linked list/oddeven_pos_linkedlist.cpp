a8635c590a79cdc5e2c528d115c751530b5084aa
Joydeep Mallick <joydeep150703@gmail.com>
2025-01-28 02:27:44 +0530

    add function to rearrange odd and even nodes in a linked list

---
+++ linked list/oddeven_pos_linkedlist.cpp
@@ -0,0 +1,31 @@
+/****************************************************************
+Following is the Linked list node structure already written
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+    Node(int data)
+    { 
+        this->data = data;
+        next = NULL;
+    }
+};
+*****************************************************************/
+
+Node* oddEvenList(Node* head)
+{
+    if(head==nullptr || head->next==nullptr) return head;
+    Node *odd = head, *even = head->next, *evenHead = head->next;
+    while(even != nullptr && even->next != nullptr){
+            odd->next = odd->next->next;
+            odd = odd->next;
+      
+            even->next =even->next->next;
+            even = even->next;
+        
+    }
+    odd->next = evenHead;
+    return head;
+}
