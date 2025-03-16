336d6dfeee3899977ff74b68c6f8bb8fb3159978
JoydeepMallick <joydeep150703@gmail.com>
2025-03-16 22:47:07 +0530

    add: implement searchRange function to find first and last position of an element in a sorted array

---
+++ binary search/find_first_and_last_position_of_element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& nums, int target) {
+            int lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+            if(lb == nums.size() || nums[lb] != target){
+                return {-1,-1};
+            }
+            //hence upper bound exist
+            int ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+            return {lb, ub-1};
+        }
+    };
