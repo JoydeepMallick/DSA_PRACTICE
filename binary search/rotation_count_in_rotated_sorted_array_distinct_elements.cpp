bf762b2890023dfc6dced627a577ea65204185ee
JoydeepMallick <joydeep150703@gmail.com>
2025-04-09 00:31:52 +0530

    rotation count

---
+++ binary search/rotation_count_in_rotated_sorted_array_distinct_elements.cpp
@@ -0,0 +1,25 @@
+int findKRotation(vector<int> &arr){
+    //right rotated r times meaning min shifted by r times to right
+    //find position of min mean found r
+    int n = arr.size(),low = 0, high = n-1;  
+    int minind = n;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        //check if left is sorted
+        if(arr[low] <= arr[mid]){
+            if(minind == n || arr[low] < arr[minind]){
+                minind = low;
+            }
+            //jump to unsorted
+            low = mid+1;
+        }
+        //check if right is sorted
+        else if(arr[mid] <= arr[high]){
+            if(minind == n || arr[mid] <= arr[minind]){
+                minind = mid;
+            }
+            high = mid-1;
+        }
+    }
+    return minind;
+}
