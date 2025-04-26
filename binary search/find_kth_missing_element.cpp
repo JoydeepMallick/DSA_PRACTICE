5911893368467b8853992a9a6361e71ad099ba35
JoydeepMallick <joydeep150703@gmail.com>
2025-04-26 21:43:47 +0530

    find kth missing element

--- binary search/find_kth_missing_element.cpp
+++ binary search/find_kth_missing_element.cpp
@@ -1,3 +1,5 @@
+// o(n) solution
+
 class Solution {
     public:
         int findKthPositive(vector<int>& arr, int k) {
@@ -8,3 +10,21 @@ class Solution {
             return ans;
         }
     };
+
+
+    //o(log n) solution
+    class Solution {
+        public:
+            int findKthPositive(vector<int>& arr, int k) {
+                int n = arr.size();
+                int low = 0, high = n-1;
+                while(low <= high){
+                    int mid = low + (high - low)/2;
+                    int misscnt = arr[mid] - mid-1;
+                    if(misscnt >= k) high = mid-1;
+                    else low = mid+1;
+                }
+                int ans = k + high + 1;
+                return ans;
+            }
+        };  
