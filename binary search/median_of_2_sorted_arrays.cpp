71438d1347f2ae6570c17afec53209b9f4ede66f
Joydeep Mallick <94801952+JoydeepMallick@users.noreply.github.com>
2025-05-02 10:48:41 +0530

    Update median_of_2_sorted_arrays.cpp

--- binary search/median_of_2_sorted_arrays.cpp
+++ binary search/median_of_2_sorted_arrays.cpp
@@ -44,3 +44,34 @@ class Solution {
 
 
     // binary search
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);//smaller array is considered for partition for lesser calculations
+
+        int n = n1+n2;
+        int low = 0, high = n1;
+        while(low <= high){
+            int mid1 = (low + high)/2; // or (low + high + 1)/2;
+            int mid2 = n/2 - mid1;
+            int l1 = (mid1 - 1 >= 0) ? nums1[mid1-1] : INT_MIN, 
+                l2 = (mid2 - 1 >= 0) ? nums2[mid2-1] : INT_MIN;
+            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX, 
+                r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
+            if(l1 <= r2 && l2 <= r1){
+                if(n&1){
+                    //left side has lesser elements
+                    return min(r1,r2); // or max(l1,l2)
+                }else{
+                    return (double)(max(l1,l2) + min(r1,r2))/2.0;
+                }
+            }else if(l1 > r2){
+                high = mid1-1;
+            }else{
+                low = mid1+1;
+            } 
+        }
+        return 0;
+    }
+};
