805f61d119dc493e77e8c37bdb4064442fede448
JoydeepMallick <joydeep150703@gmail.com>
2025-08-20 22:04:29 +0530

    disjoint sets both union by rank and size

--- graph/MST/DISJOINT_SETS.cpp
+++ graph/MST/DISJOINT_SETS.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     // initialize all containers using constructor
@@ -16,6 +16,7 @@ public:
         {
             parent[i] = i;
         }
+        size.resize(n + 1, 1); // size of each set is initially 1
     }
 
     // find ultimate parent of given node with path compression
@@ -35,5 +36,57 @@ public:
         int ulp_v = findUPar(v);
         if (ulp_u == ulp_v)
             return; // already in same set
+
+        // smaller ranked node gets attached to larger ranked node, meaning larger ranked node is parent of larger ranked node
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            // both ranks equal so any one can attach to other, rank increment happens
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++; // larger ones height increase
+        }
     }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return; // already in same set
+        // smaller sized component joins larger one
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+int main()
+{
+    DisjointSet ds(7);
+    ds.unionByRank(1, 2);
+    ds.unionByRank(2, 3);
+    ds.unionByRank(4, 5);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+    cout << "1 and 5 are in the same set: " << (ds.findUPar(1) == ds.findUPar(5)) << endl;
+    cout << "1 and 3 are in the same set: " << (ds.findUPar(1) == ds.findUPar(3)) << endl;
+
+    ds.unionByRank(3, 4);
+    cout << "1 and 5 are in the same set: " << (ds.findUPar(1) == ds.findUPar(5)) << endl;
+    cout << "2 and 6 are in the same set: " << (ds.findUPar(2) == ds.findUPar(6)) << endl;
+    cout << "3 and 7 are in the same set: " << (ds.findUPar(3) == ds.findUPar(7)) << endl;
 }
