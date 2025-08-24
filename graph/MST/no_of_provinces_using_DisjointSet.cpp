e29b34e0ee08500bd31b81c609211b9b605e6566
JoydeepMallick <joydeep150703@gmail.com>
2025-08-24 11:34:07 +0530

    update

--- graph/MST/no_of_provinces_using_DisjointSet.cpp
+++ graph/MST/no_of_provinces_using_DisjointSet.cpp
@@ -1,4 +1,111 @@
-// concept and template for disjoint sets
+// method 2 : faster
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+public:
+    vector<int> rank, parent, size;
+
+    // initialize all containers using constructor
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+        size.resize(n + 1, 1); // size of each set is initially 1
+    }
+
+    // find ultimate parent of given node with path compression
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]); // this causes path compression along the search
+    }
+
+    // union by rank
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return; // already in same set
+
+        // smaller ranked node gets attached to larger ranked node, meaning larger ranked node is parent of larger ranked node
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_u] > rank[ulp_v])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            // both ranks equal so any one can attach to other, rank increment happens
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++; // larger ones height increase
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return; // already in same set
+        // smaller sized component joins larger one
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution
+{
+public:
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+
+        int V = isConnected.size();
+        DisjointSet ds(V);
+
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                if (i != j && isConnected[i][j])
+                    ds.unionBySize(i + 1, j + 1);
+            }
+        }
+
+        int cnt = 0;
+        for (int node = 1; node <= V; node++)
+        {
+            if (ds.parent[node] == node)
+                cnt++;
+        }
+
+        return cnt;
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// MEthod 1 : Slower
 #include <bits/stdc++.h>
 using namespace std;
 
