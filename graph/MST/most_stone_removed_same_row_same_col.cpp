560805a584415200889d6636840b9370abe36636
JoydeepMallick <joydeep150703@gmail.com>
2025-09-15 00:00:02 +0530

    most stones removed

---
+++ graph/MST/most_stone_removed_same_row_same_col.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
+
+#include <bits/stdc++.h>
+
+class DisjointSet
+{
+
+public:
+    vector<int> parent, size;
+
+    DisjointSet(int n)
+    {
+        // initialize
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+        size.resize(n + 1, 1);
+    }
+
+    int findUPar(int node)
+    {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u), ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        else if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
