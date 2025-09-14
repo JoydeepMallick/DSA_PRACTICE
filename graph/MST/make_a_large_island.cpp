43390afc8d703a76272bae37e3fe03e16d571562
JoydeepMallick <joydeep150703@gmail.com>
2025-09-14 12:04:54 +0530

    making a large island

--- graph/MST/make_a_large_island.cpp
+++ graph/MST/make_a_large_island.cpp
@@ -1,12 +1,11 @@
-// problem : https://leetcode.com/problems/making-a-large-island/description/
-
 #include <bits/stdc++.h>
 
 class DisjointSet
 {
-    vector<int> parent, size;
 
 public:
+    vector<int> parent, size;
+
     DisjointSet(int n)
     {
         // initialize
@@ -38,5 +37,74 @@ public:
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
+    }
+};
+
+class Solution
+{
+public:
+    int largestIsland(vector<vector<int>> &grid)
+    {
+        // n x n binary grid
+        int n = grid.size();
+        // prepare a DS of 1s
+        DisjointSet ds(n * n);
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                if (grid[r][c] == 0)
+                    continue;
+
+                int dr[] = {1, 0, -1, 0}, dc[] = {0, 1, 0, -1};
+                for (int i = 0; i < 4; i++)
+                {
+                    int nr = r + dr[i], nc = c + dc[i];
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 1)
+                    {
+                        int node = r * n + c;
+                        int adjnode = nr * n + nc;
+                        ds.unionBySize(node, adjnode);
+                    }
+                }
+            }
+        }
+
+        // now traverse all 0s, try searching for adjacent lands and compute max answer
+        int ans = 0;
+        for (auto sz : ds.size)
+        {
+            ans = max(ans, sz);
+        }
+
+        for (int r = 0; r < n; r++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                if (grid[r][c] == 1)
+                    continue;
+
+                int dr[] = {1, 0, -1, 0}, dc[] = {0, 1, 0, -1};
+                set<int> unique_ulp;
+                for (int i = 0; i < 4; i++)
+                {
+                    int nr = r + dr[i], nc = c + dc[i];
+                    if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 1)
+                    {
+                        int adjnode = nr * n + nc;
+                        unique_ulp.insert(ds.findUPar(adjnode));
+                    }
+                }
+
+                int tempans = 1; // that 0 was assumed to be converted to land
+                for (auto ulp : unique_ulp)
+                {
+                    tempans += ds.size[ulp];
+                }
+
+                ans = max(ans, tempans);
+            }
+        }
+        return ans;
     }
 };
