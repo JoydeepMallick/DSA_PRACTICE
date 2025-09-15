11536b70977bae11557c1334b224aa258bb24a39
JoydeepMallick <joydeep150703@gmail.com>
2025-09-15 22:53:13 +0530

    most stones removed same row same column

--- graph/MST/most_stone_removed_same_row_same_col.cpp
+++ graph/MST/most_stone_removed_same_row_same_col.cpp
@@ -1,7 +1,3 @@
-// https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
-
-#include <bits/stdc++.h>
-
 class DisjointSet
 {
 
@@ -39,5 +35,50 @@ public:
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
+    }
+};
+
+class Solution
+{
+public:
+    int removeStones(vector<vector<int>> &stones)
+    {
+        // figuring out the maximum dimensions we are bound to check
+        int maxRow = 0, maxCol = 0;
+        int n = stones.size();
+
+        for (auto it : stones)
+        {
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+
+        DisjointSet ds(maxRow + maxCol + 2); // reason being indexes are 0 based and we need count of rows and cols.
+
+        // here row 0 = node 0, row 1 = node 1,.....
+        // then col 0 = node (r+1), col 1 = node (r+2),.....
+        // since we use continuous numbering for col as nodes, so
+        // col no = rowval + colval + 1 (in 0 based nomenclature)
+        for (auto it : stones)
+        {
+            int x = it[0], y = it[1];
+            int nodeRow = x, nodeCol = y + maxRow + 1;
+            // merge nodes
+            ds.unionBySize(nodeRow, nodeCol);
+        }
+
+        int validcompcnt = 0;
+        for (int node = 0; node <= maxRow + maxCol + 2; node++)
+        {
+            if (ds.findUPar(node) == node && ds.size[node] >= 2)
+            {
+                // valid component and ultimate parent of a component
+                validcompcnt++;
+            }
+        }
+
+        int maxstonesremoved = n - validcompcnt;
+
+        return maxstonesremoved;
     }
 };
